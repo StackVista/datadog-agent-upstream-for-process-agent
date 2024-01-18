cb2b4bdb57c9c3e2051f93e46850ea33b189af49
Jan-Gerd Tenberge <jtenberge@stackstate.com>
2024-01-18 11:50:08 +0000

    log_debug only when DEBUG is defined

--- pkg/ebpf/c/bpf_helpers_custom.h
+++ pkg/ebpf/c/bpf_helpers_custom.h
@@ -5,11 +5,16 @@
 
 /* Macro to output debug logs to /sys/kernel/debug/tracing/trace_pipe
  */
+#ifdef DEBUG
 #define log_debug(fmt, ...)                                        \
     ({                                                             \
         char ____fmt[] = fmt;                                      \
         bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
     })
+#else
+// No op
+#define log_debug(fmt, ...)
+#endif
 
 /* llvm builtin functions that eBPF C program may use to
  * emit BPF_LD_ABS and BPF_LD_IND instructions
