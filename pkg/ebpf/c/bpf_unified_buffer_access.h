162f5a9b445320390cc22150ca7e2f8394b8d8a5
Jan-Gerd Tenberge <jtenberge@stackstate.com>
2024-03-07 16:16:31 +0000

    Add some safeguards for the verifier

--- pkg/ebpf/c/bpf_unified_buffer_access.h
+++ pkg/ebpf/c/bpf_unified_buffer_access.h
@@ -26,11 +26,16 @@ typedef struct {
 // The `offset` parameter is in addition to any `data_offset` specified in `buffer`.
 // Returns 0 on success, or a negative error code on failure.
 static __always_inline int bpf_load_data(const bpf_buffer_desc_t *buffer, __u32 offset, void *to, __u32 len) {
-    if (buffer->type == BPF_BUFFER_TYPE_SKB) {
-        return bpf_skb_load_bytes_with_telemetry(buffer->ptr, buffer->data_offset + offset, to, len);
-    } else if (buffer->type == BPF_BUFFER_TYPE_USER) {
-        return bpf_probe_read_user_with_telemetry(to, len, buffer->ptr + buffer->data_offset + offset);
-    } else {
+	if (buffer == NULL || buffer->ptr == NULL || to == NULL) {
+		log_debug("bpf_load_data: invalid descriptor (%p) or buffer (%p) or target (%p)", buffer, buffer->ptr, to);
+		return -EINVAL;
+	}
+	
+	if (buffer->type == BPF_BUFFER_TYPE_SKB) {
+		return bpf_skb_load_bytes_with_telemetry(buffer->ptr, buffer->data_offset + offset, to, len);
+	} else if (buffer->type == BPF_BUFFER_TYPE_USER) {
+		return bpf_probe_read_user_with_telemetry(to, len, buffer->ptr + buffer->data_offset + offset);
+	} else {
 		log_debug("bpf_load_data: unknown buffer type %d", buffer->type);
 		return -EINVAL;
 	}
