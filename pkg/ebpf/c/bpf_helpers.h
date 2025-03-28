6b35a7091dd0cfabd474cab4e5c81efb0a5fe580
Louis Parkin <louis.parkin@suse.com>
2025-03-28 11:33:48 +0100

    STAC-22418 Try replacing inline assembly with C code.

--- pkg/ebpf/c/bpf_helpers.h
+++ pkg/ebpf/c/bpf_helpers.h
@@ -318,23 +318,7 @@ comparisons based on integer arithmatic. To do control flow it is still required
 This function produces 0 when equal and 1 when unequal, of type __u8.
 */
 #define __bpf_no_branch_cmp_unequal(input, comp) \
-     ({ \
-        __u64 unequal = (input) ^ (comp); \
-        __u64 tmp_unequal_boolean; \
-        __u8 unequal_boolean; \
-        /* Normalize the inequality to 1 or 0 using division. \
-           This is done via inline assembly because division by 0 is defined in BPF, \
-           but not by Clang, which would otherwise optimize undefined behavior away. */ \
-        asm volatile( \
-                  "%[tmp] = %[unequal]\n\t" \
-                  "%[tmp] /= %[unequal]\n\t" \
-                  : [tmp] "=r"(tmp_unequal_boolean) /* Use "=r" constraint */ \
-                  : [unequal] "r"(unequal) \
-                  ); \
-        /* Assign the lower 8 bits to maintain __u8 type */ \
-        unequal_boolean = (tmp_unequal_boolean != 0) ? 1 : 0; \
-        unequal_boolean; \
-     })
+    ((__u8)(!!((input) ^ (comp))))
 
 #define __bpf_no_branch_neg(v) (1 - (v))
 
